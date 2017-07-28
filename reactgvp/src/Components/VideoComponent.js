9063864ee990754c36aff050c1ea7791655da8e6
Nic Vogt <nicvogt@outlook.com>
2017-07-28 11:16:42 -0700

    basic component testing

---
+++ reactgvp/src/Components/VideoComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class VideoComponent extends Component {
+  render() {
+    return (
+      <div>
+          <h2>Video Component</h2>
+      </div>
+    );
+  }
+}
+
+export default VideoComponent;
